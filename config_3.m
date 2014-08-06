8f6248e6f6b93f0d5a5be0ef4d5fc76224bf7b8f
Sean Connolly <connolly.st@gmail.com>
2014-08-06 18:17:32 +0200

    Scaled down version of config_2

---
+++ config_3.m
@@ -0,0 +1,11 @@
+function config = config_3
+    config.size              = 25;
+    config.excitation.weight = 0.5;
+    config.excitation.length = 5;
+    config.excitation.width  = 2;
+    config.excitation.offset = 5;
+    config.inhibition.weight = 0.5;
+    config.inhibition.length = 5;
+    config.inhibition.width  = 3;
+    config.inhibition.offset = 5;
+end
