cd203d369565ffbc92eadbf628153172c23cdf7d
Sean Connolly <connolly.st@gmail.com>
2014-08-08 18:54:35 +0200

    Extracted out center/surround (w/ config)

--- SO.m
+++ SO.m
@@ -1,53 +1,50 @@
-function [pLo, mLo, pMo, mMo, pSo, mSo] = SO( I, varargin )
-%pLGN Parvocellular Lateral Geniculate Nucleus (pLGN) simulation
-%   Simulate the parvocellular lateral geniculate nucleus (pLGN) processing
-%   of the input color image.
-%       I:          the input RGB image
-%       'display':  (optional) enabled displaying the channels
+function [Lon, Loff, Mon, Moff, Son, Soff] = SO(I, config)
+%SO Lateral Geniculate Nucleus (LGN) single-opponent (SO) transformation.
+%   Transforms the input image in a manner inspired by the single-opponent
+%   cells identified in the parvocellular and koniocellular lateral
+%   geniculate nucleus (pLGN).
+%   Reference: Chalupa & Werner The Visual Neurosciences, Vol 2, p1009
+%       I:      the input RGB image
+%       config: struct array of algorithm configuration
 
-    L = I(:,:,1); % Long wavelength:   red
-    M = I(:,:,2); % Middle wavelength: green
-    S = I(:,:,3); % Short wavelength:  blue
-    Lc = center(L);
-    Mc = center(M);
-    Sc = center(S);
-    Ls = surround(L);
-    Ms = surround(M);
-    Ss = surround(S);   % TODO any reference to short surround in lit?
+    L   = I(:,:,1); % Long wavelength:   red
+    M   = I(:,:,2); % Middle wavelength: green
+    S   = I(:,:,3); % Short wavelength:  blue
     
-    %% Residuals account for information not in centers & surrounds
+    Lc  = center(L, config);
+    Mc  = center(M, config);
+    Sc  = center(S, config);
+    Ls  = surround(L, config);
+    Ms  = surround(M, config);
+    LMs = Ls .* Ms;
+    % Note: no S-surround in biology
+    
+    % Residuals account for information not in centers & surrounds
     Lr = L - Lc - Ls;
     Mr = M - Mc - Ms;
-    Sr = S - Sc - Ss;
+    Sr = S - Sc;
     
-    %% Plus (p**) and minus (m**) opponency channels (p1009)
-    pLo =  Lc - Ms;
-    mLo = -Lc + Ms;
-    pMo =  Mc - Ls;
-    mMo = -Mc + Ls;
-    LMs =  Ls .* Ms;
-    pSo =  Sc - LMs;
-    mSo = -Sc + LMs;
-    if utils.do_show(varargin{:})
-        utils.show_figure(1, 'pLo', pLo);
-        utils.show_figure(2, 'mLo', mLo);
-        utils.show_figure(3, 'pMo', pMo);
-        utils.show_figure(4, 'mMo', mMo);
-        utils.show_figure(5, 'pSo', pSo);
-        utils.show_figure(6, 'mSo', mSo);
-        utils.show_figure(7, 'LMs', LMs);
+    % Combine center surround signals to obtain color opponency..
+    Lon  = utils.on( Lc - Ms);
+    Loff = utils.on(-Lc + Ms);
+    Mon  = utils.on( Mc - Ls);
+    Moff = utils.on(-Mc + Ls);
+    Son  = utils.on( Sc - LMs);
+    Soff = utils.on(-Sc + LMs);
+    if utils.do_show(config)
+        utils.show_figure(1, 'Lon',  Lon);
+        utils.show_figure(2, 'Loff', Loff);
+        utils.show_figure(3, 'Mon',  Mon);
+        utils.show_figure(4, 'Moff', Moff);
+        utils.show_figure(5, 'Son',  Son);
+        utils.show_figure(6, 'Soff', Soff);
     end
 end
 
-function Ic = center(I)
-    Ic = gaussian(I, 5, 2);
+function Ic = center(I, config)
+    Ic = imfilter(I, rf.center(config), 'same');
 end
 
-function Is = surround(I)
-    Is = gaussian(I, 20, 5);
-end
-
-function filtered = gaussian(img, hsize, sigma)
-    filter   = fspecial('gaussian', [hsize hsize], sigma);
-    filtered = imfilter(img, filter, 'same');
+function Is = surround(I, config)
+    Is = imfilter(I, rf.surround(config), 'same');
 end
--- config_3.m
+++ config_3.m
@@ -1,5 +1,9 @@
 function config = config_3
     config.size              = 25;
+    config.center.size       = 5;
+    config.center.sigma      = 2;
+    config.surround.size     = 20;
+    config.surround.sigma    = 5;
     config.excitation.weight = 0.5;
     config.excitation.length = 5;
     config.excitation.width  = 2;
