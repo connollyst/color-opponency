43b00a06022b76f94812ebbe73da69a2b4ec6878
Sean Connolly <connolly.st@gmail.com>
2014-08-12 23:52:14 +0200

    Added normalization

--- +itti/SO.m
+++ +itti/SO.m
@@ -1,27 +1,43 @@
-function [R, G, B, Y] = SO(I, config)
+function [R, G, B, Y] = SO(img, config)
 
-    L = I(:,:,1); % Long wavelength:   red
-    M = I(:,:,2); % Middle wavelength: green
-    S = I(:,:,3); % Short wavelength:  blue
+    img = im2double(img);
     
-    Lc   = center(L, config);
-    Mc   = center(M, config);
-    Sc   = center(S, config);
-    Ls   = surround(L, config);
-    Ms   = surround(M, config);
-    Ss   = surround(S, config);
+    L = img(:,:,1); % Long wavelength:   red
+    M = img(:,:,2); % Middle wavelength: green
+    S = img(:,:,3); % Short wavelength:  blue
+    I = (L + M + S)/3;
+    
+    
+    Lc = center(L, config);
+    Mc = center(M, config);
+    Sc = center(S, config);
+    Ic = center(I, config);
+    Lc = normalize(Lc, Ic);
+    Mc = normalize(Mc, Ic);
+    Sc = normalize(Sc, Ic);
+    
+    Ls = surround(L, config);
+    Ms = surround(M, config);
+    Ss = surround(S, config);
+    Is = surround(I, config);
+    Ls = normalize(Ls, Is);
+    Ms = normalize(Ms, Is);
+    Ss = normalize(Ss, Is);
     
     % Combine center surround signals to obtain color opponency..
+    I = Ic - Is;
     R = utils.on(Lc - (Ms + Ss)/2);
     G = utils.on(Mc - (Ls + Ss)/2);
     B = utils.on(Sc - (Ls + Ms)/2);
     Y = utils.on((Lc + Mc)/2 - abs(Ls - Ms)/2 - Ss);
     
-    figure(1), imagesc(I)
-    figure(2), imagesc(R)
-    figure(3), imagesc(G)
-    figure(4), imagesc(B)
-    figure(5), imagesc(Y)
+    figure()
+    subplot(3, 2, 1), imshow(img);
+    subplot(3, 2, 2), imagesc(I), colormap('gray'), title('intensity');
+    subplot(3, 2, 3), imagesc(R), colormap('gray'), title('red');
+    subplot(3, 2, 4), imagesc(G), colormap('gray'), title('green');
+    subplot(3, 2, 5), imagesc(B), colormap('gray'), title('blue');
+    subplot(3, 2, 6), imagesc(Y), colormap('gray'), title('yellow');
 end
 
 function Ic = center(I, config)
@@ -30,4 +46,10 @@ end
 
 function Is = surround(I, config)
     Is = imfilter(I, rf.surround(config), 'same');
+end
+
+function n = normalize(channel, I)
+% Normalize channel (r, g, or b) by I.
+%   Note, unlike in Itti 1998, we normalize values at low luminance also.
+    n = channel./I;
 end
