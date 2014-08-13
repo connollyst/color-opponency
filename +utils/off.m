eca65cad12325717c7c52221958294779730f02d
Sean Connolly <connolly.st@gmail.com>
2014-08-13 20:48:51 +0200

    Simplified off by reusing on

--- +utils/off.m
+++ +utils/off.m
@@ -1,5 +1,3 @@
 function I_off = off( I )
-    I_off = I;
-    I_off(I_off > 0) = 0;   % remove all positive values
-    I_off = abs(I_off);     % flip negative values to positive
+    I_off = utils.on(-I);
 end
